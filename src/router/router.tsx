812b86e7495384a6789c2cf12fd2f495e4027273
teddyFernandez7 <FLT1010288@est.univalle.edu>
2025-01-22 23:48:08 -0400

    Layout version 2.5

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import AuthContainer from "../modules/auth/components/authContainer";
 import Login from "../modules/auth/components/login";
 import Register from "../modules/auth/components/register";
+import Layout from "../pages/layout/layout";
+import HomeContainer from "../modules/home/homeContainer";
 
 
 export default function Routing() {
     return (
         <Routes>
-            <Route index element={<AuthContainer children={<Login />} />} />
+            <Route index element={<Layout titleComponent={'REPUESTOS'} children={<HomeContainer/>}/>}/>
+            <Route path="/login" element={<AuthContainer children={<Login />} />} />
             <Route path="/register" element={<AuthContainer children={<Register />} />} />
         </Routes>
     )
