4c31178f3fd8446a6df5a59c5cd24626b396cc6e
JayLoKing <mendezpabel41@gmail.com>
2025-04-22 15:57:54 -0400

    refactor: auth store

--- src/router/roleBases.Route.tsx
+++ src/router/roleBases.Route.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store/store";
-import { jwtDecoder } from "../utils/jwtDecoder";
 
 
 interface RoleBasedRouteProps {
@@ -8,18 +7,15 @@ interface RoleBasedRouteProps {
 }
 
 export default function RoleBasedRoute({ allowedRoles }: RoleBasedRouteProps) {
-    const { jwt } = useAuthStore();
+    const { jwt, role} = useAuthStore();
     
 
     if (!jwt) {
         return <Navigate to="/login" replace />;
-    } else {
-        const decode = jwtDecoder(jwt);
-        const role = decode?.role;
-       
-        if (!role || !allowedRoles.includes(role)) {
-            return <Navigate to="/unauthorized" replace />;
-        }
+    } 
+
+    if (!role || !allowedRoles.includes(role)) {
+        return <Navigate to="/unauthorized" replace />;
     }
 
     return <Outlet />;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,24 +1,46 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { jwtDecoder } from "../utils/jwtDecoder"; 
 
-//Store
 type AuthStore = {
     jwt: string | null;
-    setAuthUser: (resource: string) => void;
+    role: string | null;
+    userId: number | null;
+    userName: string | null;
+    setAuthUser: (token: string) => void;
     clearUser: () => void;
-}
+};
 
-export const useAuthStore = create<AuthStore>((set) => ({
-
-    jwt: null,
-
-    setAuthUser: (resource: string) => {
-        set(() => ({
-            jwt: resource
-        }))
-    },
-    clearUser: () => {
-        set(() => ({
-            jwt: null
-        }))
-    }
-}));
+export const useAuthStore = create<AuthStore>()(
+    persist(
+        (set) => ({
+            jwt: null,
+            role: null,
+            userId: null,
+            userName: null,
+            setAuthUser: (token: string) => {
+                const decoded = jwtDecoder(token);
+                set({ 
+                    jwt: token,
+                    role: decoded.role,
+                    userId: decoded.id,
+                    userName: decoded.sub
+                });
+                localStorage.setItem("jwt", token);
+            },
+            
+            clearUser: () => {
+                set({ 
+                    jwt: null,
+                    role: null,
+                    userId: null,
+                    userName: null
+                });
+                localStorage.removeItem("jwt");
+            },
+        }),
+        {
+            name: "auth-storage",
+        }
+    )
+);
